5dcf9a438da8802c72f809e0807a82d03621c315
ersincodes <ersin.bahar@bilgeadam.com>
2024-02-28 19:22:14 +0300

    Middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/clerk"],
+  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
